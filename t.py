e3e9396f540a67acc065fa03b3453c98c4a79b53
bendergrim <maximevella@gmail.com>
2015-12-30 17:56:58 +0100

    Update t.py

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ oInput = int(input("Saisissez un nombre afin de voir sa table de multiplication:
 oInput2 = int(input("Saisissez le multiplicateur maximum: "))
 for i in range (oInput2):
     print (str(i),":", str(oInput), "*", str(i), "=", oInput*i)
-oInput3 = input("Voulez-vous quitter le programme? o/n")
+oInput3 =""
 while not oInput3 == "o":
-    oIpunt3
+    oInput3 = input("Voulez-vous quitter le programme? o/n")
 print "Aurevoir!"
